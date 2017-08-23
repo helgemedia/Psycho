2ae211f91db84ce02c94b4b83b2cb786d98750dd
dynerator <dynerator@googlemail.com>
2017-08-23 03:14:29 +0200

    MA_M_23082017_Anpassung_Menue

--- js/index.js
+++ js/index.js
@@ -1,15 +1,11 @@
-var open = 0;
-function toggleMenu() {
-  if (open == 0) {
+
+function closeMenu() {
+    document.getElementById("mainNav").className = "closed";
+}
+
+function openMenu() {
     document.getElementById("mainNav").className = "open";
     document.getElementById("mainNav").style.zIndex = "10";
-
-    open = 1;
-  }
-  else if (open == 1) {
-    document.getElementById("mainNav").className = "closed";
-    open = 0;
-  }
 }
 
 //Email Adress Anti-Spam var
